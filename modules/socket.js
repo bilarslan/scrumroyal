8e9f6aa53be6c0d27cdb948e265afaa5fe88af40
Bilal ARSLAN <bilarslan@windowslive.com>
2018-11-27 21:30:45 +0300

    socket module

--- modules/socket.js
+++ modules/socket.js
@@ -1,11 +1,20 @@
 module.exports = {
+    planningSessions:[],
     socket: function (io) {
+
+
         io.on('connection', function (socket) {
             console.log('User connected');
 
             socket.on('disconnect', function (socket) {
                 console.log('user disconnected');
             });
+
+
+            socket.on('newPlanningSession', function() );
         });
+    },
+    getNumberOfSessions: function(){
+        return this.planningSessions.length;
     }
 };
