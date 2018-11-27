baaa9360d636070c3227b4fe51aa1e94c2a686e6
Bilal ARSLAN <bilarslan@windowslive.com>
2018-11-27 20:08:28 +0300

    hello world socketio

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/hi', function (req, res) {
     res.send('hello world');
 });
 
-app.listen(PORT, function () {
+
+io.on('connection', function(socket){
+    console.log('User connected');
+
+    socket.on('disconnect', function(socket){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(PORT, function () {
     console.log('Server is running at ' + PORT);
 });
